672b8b36318b0c7215e9e4c681c4dcb030334cd9
cycyee <cycyee@ucdavis.edu>
2024-11-01 19:17:56 -0700

    headers

--- index.py
+++ index.py
@@ -14,7 +14,14 @@ def scrape_posts(input_search):
     url = f'https://www.taxliens.com/listing/search.html?q={input_search}'
     print(url)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Referer": "https://www.taxliens.com/",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+    
     }
     response = requests.get(url, headers = headers)
     if response.status_code == 200:
